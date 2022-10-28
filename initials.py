8403b4bffb8d01d22c16aa51b448f98643101261
Churchil Owino <chachowino75@gmail.com>
2022-10-28 11:16:30 +0300

    Initial commit

---
+++ initials.py
@@ -0,0 +1,10 @@
+#My name is Churchil Owino
+#I love coding
+
+print(" CCC H   H")
+print("C   CH   H")
+print("C    H   H")
+print("C    HHHHH")
+print("C    H   H")
+print("C   CH   H")
+print(" CCC H   H")
